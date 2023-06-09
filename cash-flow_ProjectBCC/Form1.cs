af366c5a29bbb14eb1c6d91c6beb00c4b7f2a231
Davron <88428144+wakhram@users.noreply.github.com>
2023-06-09 11:26:15 +0600

    Добавьте файлы проекта.

---
+++ cash-flow_ProjectBCC/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cash_flow_ProjectBCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
